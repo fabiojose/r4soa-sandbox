5afba2ea9c9f686c548ea77d1049e1877ef1ea35
Sandbox Sync <support@getsandbox.com>
2015-05-27 11:19:43 -0400

    Changes

--- main.js
+++ main.js
@@ -12,3 +12,8 @@ var hello11 = require("./routes/hello11.js")
  */
 Sandbox.soap("/hello11", "https://ready4soa-fabiojose.rhcloud.com/hello11/doGreeting", "RequestElement", hello11.RequestElement);
 
+Sandbox.define("/hello11/WSDL", "GET", function(req, res){
+    
+    res.jon({status : "ok"});
+    
+});
